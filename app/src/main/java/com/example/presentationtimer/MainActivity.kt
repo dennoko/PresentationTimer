e55e9c6c70e559da32c9b7bfb0c0f5b5c170e9b4
dennoko <sd514dennoko@gmail.com>
2024-04-11 15:30:50 +0900

    メイン画面の準備

--- app/src/main/java/com/example/presentationtimer/MainActivity.kt
+++ app/src/main/java/com/example/presentationtimer/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.presentationtimer.ui.theme.PresentationTimerTheme
+import com.example.presentationtimer.view.MainScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,25 +23,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MainScreen()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PresentationTimerTheme {
-        Greeting("Android")
     }
 }
